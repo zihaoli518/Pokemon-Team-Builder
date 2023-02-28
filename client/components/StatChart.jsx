ca567a772a471c15cf274cda8cc6734af050ff55
zihaoli518 <zihaoli518@gmail.com>
2023-02-27 22:30:23 -0800

    figured out color value conversion

--- client/components/StatChart.jsx
+++ client/components/StatChart.jsx
@@ -14,6 +14,8 @@ import React, { useState, useEffect } from 'react';
 import { Chart } from 'chart.js/auto';
 import { connect } from 'react-redux';
 
+// global constant for maximum number of the spectrum 
+const maxStat = 140; 
 
 const mapStateToProps = state => {
   return {
@@ -21,9 +23,22 @@ const mapStateToProps = state => {
   }
  }
 
+ // helper function for generating color based on value 
+ function stat2color(perc, maxStat) {
+   perc = perc/(maxStat/100)
+   var r,g,b = 0;
+   if (perc < 50) {
+     r = 255;
+     g = Math.round(5.1 * perc);
+   } else {
+     g = 255;
+     r = Math.round(510 - 5.1 * perc);
+   }
+   var h = r * 0x10000 + g * 0x100 + b * 0x1;
+   return "#" + ("000000" + h.toString(16)).slice(-6);
+ }
 
-let color = 'rgb(162, 0, 0)'
-
+// configurations for chart.js  
 const labels = ['HP', 'Atack', 'Defense', 'Sp. Atk.', 'Sp. Def', 'Speed'];
 const data = {
   labels: labels,
@@ -60,6 +75,7 @@ const config = {
   },
 };
 
+// main React component 
 const StatChart = props => {
 
   const [startChart, setStartChart] = useState(false);
@@ -70,7 +86,14 @@ const StatChart = props => {
     for (let key in props.currentPokemon.stats) {
       newStats.push(props.currentPokemon.stats[key]);
     }
-    data.datasets[0].data = newStats
+    data.datasets[0].data = newStats;
+    // generating color for each stat 
+    const newColors = [];
+    for (let i=0; i<newStats.length; i++) {
+      newColors.push(stat2color(newStats[i], maxStat));
+    }
+    console.log('newColors ', newColors)
+    data.datasets[0].backgroundColor = newColors;
   }
 
 
@@ -94,12 +117,6 @@ const StatChart = props => {
     const parent = document.getElementById('stat-chart-' + props.name);
     parent.append(newCanvas);
   }
-
-
-  // myChart = new Chart(
-  //   document.getElementById("myChart"), 
-  //   config
-  // );
 
   return (
     <div className='stat-chart' id={'stat-chart-' + props.name}>
