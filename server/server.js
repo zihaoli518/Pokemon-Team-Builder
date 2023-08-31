09c785607a16979837c718a72c72897f64c61f3f
zihaoli518 <zihaoli518@gmail.com>
2023-08-31 00:25:24 -0700

    added more headers

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.use((req, res, next) => {
   console.log('inside first app.use, ');
 
   res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   // check for user permission with cookies 
   if (req.cookies.PokemonTeamBuilder)  {
     res.header('cookie', req.cookies);
